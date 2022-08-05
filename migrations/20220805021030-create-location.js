e782c830495fa649cdf1c84134642224be3d0fe3
Fatimah Abdolcader <fatimah.abdolcader@gmail.com>
2022-08-04 19:18:45 -0700

    creating new branch

---
+++ migrations/20220805021030-create-location.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Locations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      id: {
+        type: Sequelize.INTEGER,
+      },
+      Name: {
+        type: Sequelize.STRING,
+      },
+      Address: {
+        type: Sequelize.STRING,
+      },
+      LocationUserId: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Locations');
+  },
+};
