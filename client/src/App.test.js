0e1d7ddd406edf8ed569546f3c90b12bd8abe4a5
Francis Li <mail@francisli.com>
2023-02-22 17:44:29 -0800

    Remove now irrelevant test

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders home', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Full Stack Starter/i);
-  expect(linkElement).toBeInTheDocument();
-});
